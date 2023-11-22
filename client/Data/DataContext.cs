c9becc61a9037cd7c57ec30b9fef034c3b9eae7a
Charlie Skibsted <charlieskibsted@gmail.com>
2023-11-22 16:56:31 +0100

    beginning of database

---
+++ client/Data/DataContext.cs
@@ -0,0 +1,16 @@
+﻿using client.EnergyMeter;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Identity.Client;
+
+namespace client.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+            
+        }
+
+
+    }
+}
